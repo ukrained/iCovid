8f6c191b0451ed707a93d4e527caf7dd7bc531c8
Alex Viytiv <sviytiv@gmail.com>
2020-04-18 18:22:42 +0300

    Оновлення iCovid / Робота з мережею - Додано взаємодію з мережевими ресурсами. - Додано оновлення даних з мережі. - Додано функції до системи логування. - Невеликі зміни та виправлення.

--- icovid.py
+++ icovid.py
@@ -2,7 +2,7 @@
 
 # metadata
 __title__ = 'iCovid Monitoring Utility'
-__version__ = '0.1.0[a]'
+__version__ = '0.1.4[a]'
 __release__ = '17 Apr 2020'
 __author__ = 'Oleksandr Viytiv'
 
@@ -11,6 +11,8 @@ import urllib.request
 import urllib.parse
 import re
 
+from lxml import html
+from datetime import datetime
 from utils import colour, logLevel, logger
 
 
@@ -30,7 +32,7 @@ class iCovidBase:
 
         return html.decode('utf-8')
 
-    def html_get_node(self, html, pattern, nid=None):
+    def html_get_node(self, html_buffer, pattern, nid=None):
         ''' Function lookup HTML content
 
         :param html: WEB page HTML data
@@ -38,12 +40,10 @@ class iCovidBase:
         :param nid: Node ID if user want specific node
         :return: all nodes found
         '''
-        nodes = re.findall(r'<p>(.*?)</p>', str(html))
+        tree = html.fromstring(html_buffer)
+        nodes = tree.xpath(pattern)
 
-        # for item in paragraphs:
-        #    text = 'Block:\n{}\n'.format(item)
-        #    print(text)
-        return nodes[nid] if nid else nodes
+        return nodes[nid] if nid is not None else nodes
 
 
 class iCovid (iCovidBase):
@@ -51,14 +51,46 @@ class iCovid (iCovidBase):
         ''' Constructor '''
         super().__init__()
         self.config = {'country': 'Україна', 'popl': 41880000, 'area': 603628,
-                       'tested': 47096, 'sick': 4662, 'recovered': 246, 'dead': 125,
+                       'tested': 0, 'sick': 0, 'recovered': 0, 'dead': 0,
                        'regions': {}}
+        self._updated = 0
 
     def update(self):
         ''' Update latest data '''
-        # 'oblasti': 'https://moz.gov.ua/article/news/operativna-informacija-pro-poshirennja-koronavirusnoi-infekcii-2019-ncov-',
-        # 'zagalom': 'https://covid19.gov.ua/'
-        pass
+        self.logger.normal('Оновлення даних ..')
+
+        self.__update_total()
+        self.__update_regions()
+
+        self.logger.success('Оновлення виконано')
+        self._updated = datetime.now()
+
+    def __update_total(self):
+        # covid19.gov.ua
+        self.logger.normal('Збір загальних даних ..')
+        page = self.web_request('https://covid19.gov.ua/')
+
+        divs = self.html_get_node(page, './/div[@class="one-field light-box info-count"]')
+        if len(divs) != 4:
+            self.logger.error('Not expected number of nodes - %d' % len(divs))
+            exit(1)
+
+        for i, case in enumerate(['tested', 'sick', 'recovered', 'dead']):
+            self.config[case] = divs[i].xpath('.//div')[0].text.strip()
+
+        self.logger.success('Загальні дані оновлено')
+
+    def __update_regions(self):
+        # moz.gov.ua
+        self.logger.normal('Збір даних про регіони ..')
+        page = self.web_request('https://moz.gov.ua/article/news/operativna-informacija-pro-poshirennja-koronavirusnoi-infekcii-2019-ncov-')
+
+        regns = self.html_get_node(page, './/div[@class="editor"]//ul', nid=0)
+        for region in regns:
+            reg, cases = region.text.split(' — ')
+            self.config['regions'][reg] = int(cases.strip().split()[0])
+
+        self.logger.success('Інформацію про регіони оновлено')
 
     def __str__(self):
         ''' Show COVID information '''
@@ -73,7 +105,9 @@ class iCovid (iCovidBase):
         dead = self.config.get('dead', '?')
         regions = self.config.get('regions', {})
 
-        text = '\n'
+        # datetime object containing current date and time
+        text = ' * Дані станом на {:%d %B %Y [%H:%M:%S]}\n\n'.format(self._updated)
+
         # total information
         text += '   [ %s ] ' % self.logger.encolour(colour.fg.cyan, country)
         text += '  %s %s' % (tested,
@@ -94,19 +128,19 @@ class iCovid (iCovidBase):
         if regions:
             min_cases = min(regions.values())
             zone_step = (max(regions.values()) + 1 - min_cases) / 5
-            zone_colour = {0: colour.NORMAL, 1: colour.fg.yellow,
+            zone_colour = {0: colour.fg.white, 1: colour.fg.yellow,
                            2: colour.fg.orange, 3: colour.fg.lightred,
                            4: colour.fg.red}
 
-            line = ''
+            line = ' '
             for i, key in enumerate(regions):
                 # depending of the value, region will have its colour
                 clr = zone_colour[(regions[key] - min_cases) // zone_step]
-                line += '  {:.<40} [{}]'.format(self.logger.encolour(clr, key) + ' ',
-                                                regions[key])
+                line += '  {:.<38} {:<6}'.format(self.logger.encolour(clr, key) + ' ',
+                                                 '[' + str(regions[key]) + ']')
                 if i % 2:
                     text += line + '\n'
-                    line = ''
+                    line = ' '
             text += (line + '\n') if line else ''  # in case of odd regions
         else:
             text += '  << Немає даних по регіонах >>\n'
@@ -123,6 +157,7 @@ class iCovid (iCovidBase):
 
 def main():
     covid = iCovid()
+    covid.update()
     print(covid)
 
 
